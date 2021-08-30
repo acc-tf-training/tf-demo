2afaff01b4d065b15cd77f9c8922d8cfc4c67cdc
EC2 Default User <ec2-user@ip-172-31-14-242.eu-central-1.compute.internal>
2021-08-30 16:31:26 +0000

    updated the tf file to remove the keys

--- Lab-1/demo01.tf
+++ Lab-1/demo01.tf
@@ -2,8 +2,8 @@
 # VARIABLES
 ##################################################################################
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
 variable "region" {
@@ -15,8 +15,8 @@ variable "region" {
 ##################################################################################
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+#  access_key = var.aws_access_key
+#  secret_key = var.aws_secret_key
   region     = var.region
 }
 
