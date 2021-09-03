4d6bd7eb9522fa097ad0e282723ae65fa4830219
retheshnair <retheshnair@gmail.com>
2021-09-03 17:27:13 +0530

    azurerm output

---
+++ Lab/azurerm-output/main.tf
@@ -0,0 +1,35 @@
+
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "mygroup"
+  location = "eastus"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "CloudDhosth"
+  address_space       = ["10.0.0.0/16"]
+  location            = "Central US"
+  resource_group_name = azurerm_resource_group.example.name
+  # Create Tag
+  tags = {
+  Environment = "Dev"
+  Team        = "Cloud"
+  }
+}
