8c50893fb94c4c0ccff5ff855b385cdbd602b131
Rethesh Nair <retheshnair@gmail.com>
2021-09-04 11:48:10 +0530

    Making chnages to the files

---
+++ Terraform-Basics/lab/packer-install/install.sh
@@ -0,0 +1,2 @@
+wget https://releases.hashicorp.com/packer/1.7.4/packer_1.7.4_linux_amd64.zip
+sudo unzip packer_1.7.4_linux_amd64.zip -d /usr/local/bin
---
+++ Terraform-Basics/lab/variables/variables.tf
@@ -0,0 +1,7 @@
+variable "storage_name" {
+  description = "Name of the Storage"
+  default     = {
+                 "first" = 100
+                 "second" = "retstorage600"
+                }
+}
