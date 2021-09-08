3a90d7ab9ee0c222d74d414adf7201dcf7921670
retheshnair <retheshnair@users.noreply.github.com>
2021-09-08 11:06:58 +0530

    Update install.sh

--- Terraform-Basics/lab/terraform-install/install.sh
+++ Terraform-Basics/lab/terraform-install/install.sh
@@ -1,2 +1,2 @@
 wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
-sudo unzip packer_1.7.4_linux_amd64.zip -d /usr/local/bin
+sudo unzip terraform_1.0.5_linux_amd64.zip -d /usr/local/bin
