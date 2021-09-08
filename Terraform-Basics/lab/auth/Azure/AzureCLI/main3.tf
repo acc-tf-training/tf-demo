c21bb893e259db8945c3bdac8c2d68118e49a27e
vishnu vs <54799446+vishnuvs369@users.noreply.github.com>
2021-09-08 12:10:47 +0530

    azure auth changes

---
+++ Terraform-Basics/lab/auth/Azure/AzureCLI/main3.tf
@@ -0,0 +1,20 @@
+#If you're looking to use Terraform across Tenants - it's possible to do this by configuring the Tenant ID field in the Provider block, as shown below:
+
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "11111111-1111-1111-1111-111111111111"
+}
