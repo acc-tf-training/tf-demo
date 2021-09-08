8384976c95473fd752265635e277ca57abd94320
Rethesh Nair <retheshnair@gmail.com>
2021-09-08 16:42:36 +0530

    import example

---
+++ Terraform-Beyond-The-Basics/lab/import/azure/main.tf
@@ -0,0 +1,22 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "FILL_THE_NAME_STOARGEACC_MANUALLY_CREATED"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
