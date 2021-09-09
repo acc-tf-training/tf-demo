56e4ffbad1115d975e42f53f58ada6d7315d0b2b
Akhil Nair <akhilvnair000@gmail.com>
2021-09-09 15:42:24 +0530

    Added aws env authentication

--- Terraform-Basics/lab/auth/aws/Env/commands.sh
+++ Terraform-Basics/lab/auth/aws/Env/commands.sh
@@ -1,3 +1,8 @@
+export AWS_ACCESS_KEY_ID="AKI***************7I"
+export AWS_SECRET_ACCESS_KEY="VzH**************************476"
+export AWS_DEFAULT_REGION="us-west-2"
+
+
 terraform init
 terraform validate
 terraform plan 
