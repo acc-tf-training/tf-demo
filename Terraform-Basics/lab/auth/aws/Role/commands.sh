782faaf8ada34161ce79c32114523d72d2193f22
Akhil Nair <akhilvnair000@gmail.com>
2021-09-13 03:07:06 +0530

    added role authentication

--- Terraform-Basics/lab/auth/aws/Role/commands.sh
+++ Terraform-Basics/lab/auth/aws/Role/commands.sh
@@ -1,3 +1,11 @@
+aws iam create-role --role-name tf_admin --assume-role-policy-document file://ec2-role-trust-policy.json
+aws iam put-role-policy --role-name tf_admin --policy-name admin-access --policy-document file://ec2-role-access-policy.json
+aws iam create-instance-profile --instance-profile-name tf_admin-profile
+
+#attach the created tf_admin role to the instance which has terraform running 
+
+
+
 terraform init
 terraform validate
 terraform plan 
