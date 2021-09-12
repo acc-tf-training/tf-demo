c0da1d68cdcaa919709feaa9bf30176485793269
retheshnair <retheshnair@users.noreply.github.com>
2021-09-12 16:48:45 +0530

    Update install.sh

--- Terraform-Basics/lab/terraform-install/install.sh
+++ Terraform-Basics/lab/terraform-install/install.sh
@@ -1,2 +1,4 @@
+#install unzip command using yum install unzip or apt install unzip
+
 wget https://releases.hashicorp.com/terraform/1.0.5/terraform_1.0.5_linux_amd64.zip
 sudo unzip terraform_1.0.5_linux_amd64.zip -d /usr/local/bin
