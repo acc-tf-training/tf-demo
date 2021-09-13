1fc9f7a918d41042ecc6fb16ee8c6f780c0163db
Rethesh Nair <retheshnair@gmail.com>
2021-09-13 07:39:07 +0530

    added the files

---
+++ Terraform-Basics/lab/auth/AWS/Role/main.tf
@@ -0,0 +1,13 @@
+  
+provider "aws" {
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
