c79151d0e44ff380666880f042fc5aaec95ecc71
Rethesh Nair <retheshnair@gmail.com>
2021-09-13 07:46:59 +0530

    added the files

--- Terraform-Basics/lab/auth/AWS/Role/commands.sh
+++ Terraform-Basics/lab/auth/AWS/Role/commands.sh
@@ -1,6 +1,7 @@
 aws iam create-role --role-name tf_admin --assume-role-policy-document file://ec2-role-trust-policy.json
 aws iam put-role-policy --role-name tf_admin --policy-name admin-access --policy-document file://ec2-role-access-policy.json
 aws iam create-instance-profile --instance-profile-name tf_admin-profile
+aws iam add-role-to-instance-profile --role-name tf_admin --instance-profile-name tf_admin-profile
 
 #attach the created tf_admin role to the instance which has terraform running 
 
