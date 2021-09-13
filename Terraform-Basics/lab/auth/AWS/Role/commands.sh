9335b7f93533a5e3ae17eaa9dee10f7e45e6726b
retheshnair <retheshnair@users.noreply.github.com>
2021-09-13 08:03:26 +0530

    Update commands.sh

--- Terraform-Basics/lab/auth/AWS/Role/commands.sh
+++ Terraform-Basics/lab/auth/AWS/Role/commands.sh
@@ -1,3 +1,6 @@
+aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
+chmod 400 MyKeyPair.pem
+aws ec2 describe-key-pairs --key-name MyKeyPair
 aws iam create-role --role-name tf_admin --assume-role-policy-document file://ec2-role-trust-policy.json
 aws iam put-role-policy --role-name tf_admin --policy-name admin-access --policy-document file://ec2-role-access-policy.json
 aws iam create-instance-profile --instance-profile-name tf_admin-profile
