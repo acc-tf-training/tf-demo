025b33c6120c08ae313930a88e00589970e22777
retheshnair <retheshnair@users.noreply.github.com>
2021-09-13 08:15:07 +0530

    Update commands.sh

--- Terraform-Basics/lab/auth/AWS/Role/commands.sh
+++ Terraform-Basics/lab/auth/AWS/Role/commands.sh
@@ -1,6 +1,9 @@
 aws ec2 create-key-pair --key-name MyKeyPair --query 'KeyMaterial' --output text > MyKeyPair.pem
 chmod 400 MyKeyPair.pem
 aws ec2 describe-key-pairs --key-name MyKeyPair
+aws ec2 describe-security-groups  [ to find the sg ]
+aws ec2 describe-subnets [ to find the subnet id ] 
+aws ec2 run-instances --image-id ami-029c64b3c205e6cce --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
 aws iam create-role --role-name tf_admin --assume-role-policy-document file://ec2-role-trust-policy.json
 aws iam put-role-policy --role-name tf_admin --policy-name admin-access --policy-document file://ec2-role-access-policy.json
 aws iam create-instance-profile --instance-profile-name tf_admin-profile
