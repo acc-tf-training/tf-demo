730c99600f38d97ebeb3ca4a9d61d40395e75857
Rethesh Nair <retheshnair@gmail.com>
2021-09-13 08:45:33 +0530

    added the files

--- Terraform-Basics/lab/auth/AWS/Env/commands.sh
+++ Terraform-Basics/lab/auth/AWS/Env/commands.sh
@@ -1,6 +1,6 @@
 export AWS_ACCESS_KEY_ID="AKI***************7I"
 export AWS_SECRET_ACCESS_KEY="VzH**************************476"
-export AWS_DEFAULT_REGION="us-west-2"
+export AWS_DEFAULT_REGION="us-east-1"
 
 
 terraform init
