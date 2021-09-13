deca3def3050152c1fc200a5b4757e603b07b05b
retheshnair <retheshnair@users.noreply.github.com>
2021-09-13 08:17:48 +0530

    Update commands.sh

--- Terraform-Basics/lab/auth/AWS/Profile/commands.sh
+++ Terraform-Basics/lab/auth/AWS/Profile/commands.sh
@@ -1,3 +1,12 @@
+aws configure --profile tf-admin
+AWS Access Key ID [None]:"AKI*******7I"
+AWS Secret Access Key [None]:"VzH**********476"
+Default region name [None]: us-east-1
+Default output format [None]:
+
+
+
+
 terraform init
 terraform validate
 terraform plan 
